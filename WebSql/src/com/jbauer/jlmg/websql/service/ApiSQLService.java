ad52926eaedfac95a6494ef8f7f3db40f6ed0c57
Jose <joseluismg@gmail.com>
2018-09-25 23:28:19 +0200

    features in Feedback:  - auto fill tables  - http authentication  - cosmetic changes  - multiple databases configured in context

--- WebSql/src/com/jbauer/jlmg/websql/service/ApiSQLService.java
+++ WebSql/src/com/jbauer/jlmg/websql/service/ApiSQLService.java
@@ -1,11 +1,12 @@
 package com.jbauer.jlmg.websql.service;
 
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-
 import com.jbauer.jlmg.websql.model.QueryBody;
 
 public interface ApiSQLService {
 
-	public Response sql(QueryBody body);
+	public Response sql(@Context HttpHeaders headers,QueryBody body);
 	
 }
--- WebSql/src/com/jbauer/jlmg/websql/service/ApiSQLServiceImpl.java
+++ WebSql/src/com/jbauer/jlmg/websql/service/ApiSQLServiceImpl.java
@@ -1,24 +1,24 @@
 package com.jbauer.jlmg.websql.service;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLWarning;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.sql.DataSource;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
-import com.google.common.base.Strings;
-import com.google.common.base.Throwables;
 import com.jbauer.jlmg.websql.model.QueryBody;
 import com.jbauer.jlmg.websql.model.QueryResults;
 
@@ -30,24 +30,40 @@ public class ApiSQLServiceImpl implements ApiSQLService {
     @Override
     @POST
     @Path("/sql")
-    public Response sql(QueryBody body) {
+    public Response sql(@javax.ws.rs.core.Context HttpHeaders headers, QueryBody body) {
         QueryResults results = new QueryResults();
         Statement stmt = null;
-        ArrayList<String> headers = new ArrayList<String>();
+        ArrayList<String> tableHeaders = new ArrayList<String>();
         ArrayList<List<String>> rows = new ArrayList<List<String>>();
         ArrayList<String> logs = new ArrayList<String>();
         
+        
         try {
+            
+            String authHeader = headers.getHeaderString("authorization");
+            System.out.println("Auth decoded "+authHeader);            
+            String encodedValue = authHeader.split(" ")[1];
+            String decodedValue = new String(Base64.getDecoder().decode(encodedValue));
+            System.out.println("Auth decoded "+decodedValue);
+            String user = decodedValue.split(":")[0];
+            String pwd = decodedValue.split(":")[1];
+            
             InitialContext ic = new InitialContext();
             Context xmlContext = (Context) ic.lookup("java:comp/env");
-            DataSource ds = (DataSource) xmlContext.lookup("jdbc/testdb");
             Connection conn;
+/*            DataSource ds = (DataSource) xmlContext.lookup("jdbc/testdb");
+            
             if ((Strings.isNullOrEmpty(body.getUser()))||(Strings.isNullOrEmpty(body.getPassword()))){
                 conn = ds.getConnection();
             }else{
                  conn = ds.getConnection(body.getUser(), body.getPassword());
             }
-                
+*/
+            String dburl = (String) xmlContext.lookup("dburl");
+            String table = (String) xmlContext.lookup("table_"+user);
+            Class.forName("com.mysql.jdbc.Driver");
+        
+            conn = DriverManager.getConnection(dburl+table, user, pwd);
             
 
             stmt = conn.createStatement();
@@ -69,9 +85,9 @@ public class ApiSQLServiceImpl implements ApiSQLService {
                 ResultSetMetaData metadata = rs.getMetaData();
                 int columnCount = metadata.getColumnCount();    
                 for (int i = 1; i <= columnCount; i++) {
-                    headers.add(metadata.getColumnName(i));      
+                    tableHeaders.add(metadata.getColumnName(i));      
                 }
-                results.setHeaders(headers);
+                results.setHeaders(tableHeaders);
             
                 while (rs.next()) {
                     ArrayList<String> l= new ArrayList<String>();
@@ -86,7 +102,7 @@ public class ApiSQLServiceImpl implements ApiSQLService {
         } catch (Exception ex) {
             ex.printStackTrace();
             results.setErrorMessage(ex.getMessage());            
-            logs.add(Throwables.getStackTraceAsString (ex));            
+            //logs.add(Throwables.getStackTraceAsString (ex));            
             results.setLog(logs);
             return Response.ok(results).build();
         } finally {
